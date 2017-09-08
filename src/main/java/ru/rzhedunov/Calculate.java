7230bb371588305ea94b831e55b9c56dbae03186
rzhedunov <rzhedunov@mail.ru>
2017-09-08 22:52:29 +0300

    0. Видео.[#3520]

---
+++ src/main/java/ru/rzhedunov/Calculate.java
@@ -0,0 +1,14 @@
+package ru.rzhedunov;
+
+public class Calculate{
+	public String echo(String value){
+		return String.format("%s %s %s", value, value, value);
+	}
+
+	public static void main(String[] args){
+		Calculate calc = new Calculate();
+		System.out.println(calc.echo("aah"));
+	}
+
+
+} 
