f242022a2ecb4bbb59fda39b3eaa4e609904730d
RuslanZhedunov <rzhedunov@mail.ru>
2017-09-13 08:49:40 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,19 @@
 package ru.job4j;
 
+/**
+* Class Calculate to illustrate JavaDoc comments
+* @author rzhedunov
+* @since 2017-09-13
+* @version 2.0
+*/            
+
 public class Calculate{
+	
+	/**
+	* Main method of Calculate class
+	* @params args is first arg
+	*/
+
 	public static void main(String[] args){
 		System.out.println("Hello, world!");
 	}
