753e0665933b281ab1adad3eb315e3d6955a11ab
RuslanZhedunov <rzhedunov@mail.ru>
2017-09-17 18:17:49 +0300

    1.5. Checkstyle [#4123]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,29 +1,27 @@
 package ru.job4j;
-
 /**
-* Class Calculate to illustrate JavaDoc comments
+* Class Calculate to illustrate JavaDoc comments.
 * @author rzhedunov
 * @since 2017-09-13
 * @version 2.0
-*/            
+*/
 
-public class Calculate{
-	
+public class Calculate {
+
 	/**
-	* Main method of Calculate class
-	* @params args is first arg
+	* Main method of Calculate class.
+	* @param args is first arg.
 	*/
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		System.out.println("Hello, world!");
 	}
 
 	/**
 	* Method echo.
-	* @param name rzhedunov.
-	* @return Echo plus your name.
+	* @param name is a name of user.
+	* @return Echo plus name of user given.
 	*/
 	public String echo(String name) {
 	    return "Echo, echo, echo : " + name;
 	}
-
-} 
+}
--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -1,8 +1,8 @@
 package ru.job4j;
 
 import org.junit.Test;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
+//import java.io.ByteArrayOutputStream;
+//import java.io.PrintStream;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 /**
@@ -15,14 +15,13 @@ import static org.junit.Assert.assertThat;
 public class CalculateTest {
 	/**
 	* Test echo.
-	*/ 
+	*/
 	@Test
 	public void whenTakeNameThenTreeEchoPlusName() {
 	    String input = "Ruslan Zhedunov";
-	    String expect = "Echo, echo, echo : Ruslan Zhedunov"; 
+	    String expect = "Echo, echo, echo : Ruslan Zhedunov";
 	    Calculate calc = new Calculate();
 	    String result = calc.echo(input);
 	    assertThat(result, is(expect));
-	}	 
+	}
 }
- 
