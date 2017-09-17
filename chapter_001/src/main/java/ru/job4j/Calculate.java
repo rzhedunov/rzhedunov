19c6de50762e41a0b58582778fb6b3b5d597c4ba
RuslanZhedunov <rzhedunov@mail.ru>
2017-09-17 15:38:20 +0300

    1.4. JUnit [#4122]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -13,8 +13,17 @@ public class Calculate{
 	* Main method of Calculate class
 	* @params args is first arg
 	*/
-
 	public static void main(String[] args){
 		System.out.println("Hello, world!");
 	}
+
+	/**
+	* Method echo.
+	* @param name rzhedunov.
+	* @return Echo plus your name.
+	*/
+	public String echo(String name) {
+	    return "Echo, echo, echo : " + name;
+	}
+
 } 
