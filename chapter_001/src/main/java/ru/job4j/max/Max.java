5682116424d78dee5dbdb3fad87642c6ac9ed289
rzhedunov <rzhedunov@mail.ru>
2017-10-04 12:09:12 +0300

    4.4. Максимум из трех чисел [#190][#25505]

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -1,19 +1,30 @@
 package ru.job4j.max;
 
 /**
- * Class Max 4.1.
+ * Class Max 4.1.2.
  * @author rzhedunov.
  * @since 2017-09-21.
- * @version 4.1.
+ * @version 4.1.2.
  */
 public class Max {
     /**
-     * Method max returns the biggest of two numbers given.
+     * Method max(int first, int second) returns the biggest of two numbers given.
      * @param first is the first number
      * @param second is the second number
      * @return int
      */
     public int max(int first, int second) {
         return (first > second) ? first : second;
+    }
+
+    /**
+     * Method max(int first, int second, int third) returns the biggest of three numbers given.
+     * @param first is the first number
+     * @param second is the second number
+     * @param third is the third number
+     * @return int
+     */
+    public int max(int first, int second, int third) {
+        return max(max(first, second), third);
     }
 }
--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -5,10 +5,10 @@ import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 /**
- * Class MaxTest 4.1.
+ * Class MaxTest 4.1.2.
  * @author rzhedunov.
  * @since 2017-09-21.
- * @version 4.1.
+ * @version 4.1.2.
  */
 
 public class MaxTest {
@@ -33,13 +33,36 @@ public class MaxTest {
     }
 
     /**
-     * Method whenNumbersAreEqual tests result when numbers are equal.
+     * Method whenTwoNumbersAreEqual tests result when numbers are equal.
      */
     @Test
-    public void whenNumbersAreEqual() {
+    public void whenTwoNumbersAreEqual() {
         Max maxim = new Max();
         int result = maxim.max(1, 1);
         assertThat(result, is(1));
     }
 
+    /**
+     * Method whenThreeNumbersAreEqual tests result when three numbers are equal.
+     */
+    @Test
+    public void whenThreeNumbersAreEqual() {
+        Max maxim = new Max();
+        int result = maxim.max(1, 1, 1);
+        assertThat(result, is(1));
+    }
+
+    /**
+     * Method whenThreeNumbersAreNotEqual tests result when three numbers are not equal.
+     */
+    @Test
+    public void whenThreeNumbersAreNotEqual() {
+        Max maxim = new Max();
+        int result = maxim.max(2, 1, 1);
+        assertThat(result, is(2));
+        result = maxim.max(1, 3, 1);
+        assertThat(result, is(3));
+        result = maxim.max(1, 1, 4);
+        assertThat(result, is(4));
+    }
 }
